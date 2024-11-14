98c748e1c258c4d0bf62e3c91cdec40c6fd9f35a
daux <daux@notreal.org>
2024-11-14 22:43:27 +0100

    handling of 1000+ entries lists

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # mal-ptw-match
-
-https://malptw.daux0r.de/
-
-This web app gets two users plan-to-watch list from the MyAnimeList REST API and shows what titles match.
--- app.js
+++ app.js
@@ -44,16 +44,40 @@ app.get("/", async (req, res) => {
     
 });
 
+// https://www.geeksforgeeks.org/how-to-delay-a-loop-in-javascript-using-async-await-with-promise/
+function waitforme(millisec) {
+    return new Promise(resolve => {
+        setTimeout(() => { resolve('') }, millisec);
+    })
+}
+
 /* Get MAL users ptw-list */
 async function getPtwList(user) {
     try {
-        const res = await axios.get("https://api.myanimelist.net/v2/users/" + user + "/animelist?offset=0&limit=1000&nsfw=true&status=plan_to_watch", {
-            headers: {
-                "X-MAL-CLIENT-ID": process.env.MAL_ID // MAL API KEY
-            }
-        });
-        console.log(res.data.data);
-        return res.data.data.map(el => el.node);
+        let offset = 0;
+        let loops = 1;
+        let ptwList = [];
+        for (let i = 0; i < loops; i++) {
+                console.log(user + " request: " + (i + 1));
+
+                const res = await axios.get("https://api.myanimelist.net/v2/users/" + user + "/animelist?offset=" + offset + "&limit=1000&nsfw=true&status=plan_to_watch", {
+                    headers: {
+                        "X-MAL-CLIENT-ID": process.env.MAL_ID // MAL API KEY
+                    }
+                });
+
+                ptwList = ptwList.concat(res.data.data.map(el => el.node));
+
+                if (ptwList.length == offset + 1000) { // add iteration if at query limit
+                    loops++;
+                    offset = offset + 1000;
+                    await waitforme(1000); //add 1 sec delay between iterations in case of rate limits
+                };
+
+        }
+
+        console.log(user + " ptw: " + ptwList.length);
+        return ptwList;
     } catch (error) {
         console.log(error);
         return;
@@ -63,11 +87,14 @@ async function getPtwList(user) {
 /* Compare 2 ptw-lists and return matches */
 async function compareLists(user1, user2) {
     try {
+        console.log("-------------------------");
         const list1 = await getPtwList(user1);
         const list2 = await getPtwList(user2);
         if (list1 && list2) {
             const matches = list1.filter(anime => list2.map(el => el.id).includes(anime.id)); /* 🤕 */
-            console.log(matches.map(el => el.title));
+
+            console.log("matches: " + matches.length);
+            console.log("-------------------------");
 
             return matches;   
         } else {
